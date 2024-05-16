ea47aec558ff14187a9eee2da74ac60f523c3724
Exizygl <exizelyglaya@gmail.com>
2024-05-16 17:10:13 +0200

    create database

---
+++ GameStore.Api/Data/DataExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.Api.Data;
+
+public static class DataExtensions
+{
+ public static void MigrateDb(this WebApplication app)
+ {
+        using var scope = app.Services.CreateScope();
+ }
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,14 @@
 using GameStore.Api;
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using Microsoft.VisualBasic;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 

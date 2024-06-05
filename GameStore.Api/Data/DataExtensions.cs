5fdb1d6a181d6707aa36155b8e76c595ca99da79
Exizygl <exizelyglaya@gmail.com>
2024-06-05 23:42:25 +0200

    seeding data

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,9 +1,13 @@
-﻿namespace GameStore.Api.Data;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
 
 public static class DataExtensions
 {
  public static void MigrateDb(this WebApplication app)
  {
         using var scope = app.Services.CreateScope();
+        var DbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        DbContext.Database.Migrate();
  }
 }
